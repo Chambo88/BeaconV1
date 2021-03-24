f194bc9986a1788d4f30f7a2fb660558bd26f327
Chambo88 <benchamberlain88@gmail.com>
2021-03-24 23:22:22 +1300

    trying to learn git

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
           ),
         ],
         child: MaterialApp(
-            title: 'Flutter Deeeeemo',
+            title: 'Flutter Demo',
             theme: ThemeData(
               // This is the theme of your application.
               //
