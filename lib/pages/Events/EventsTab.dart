4a70c9a02edc9aae6fd379127ba107ccb28a4fb8
Chambo88 <benchamberlain88@gmail.com>
2021-09-07 18:54:16 +1200

    tweaks

--- lib/pages/Events/EventsTab.dart
+++ lib/pages/Events/EventsTab.dart
@@ -1,5 +1,6 @@
 import 'package:beacon/util/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class EventsTab extends StatefulWidget {
 
@@ -26,6 +27,7 @@ class _EventsTabState extends State<EventsTab> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           height:50,
@@ -33,49 +35,70 @@ class _EventsTabState extends State<EventsTab> {
             scrollDirection: Axis.horizontal,
             shrinkWrap: true,
             children: [
-              ActionChip(
-                avatar: Icon(Icons.people_outline_outlined,
-                  color: (filterBy == "Mutual")? Color(figmaColours.highlight) : Colors.white,
-                ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: ActionChip(
+                  side: BorderSide(width: 0),
+                  avatar: Padding(
+                    padding: const EdgeInsets.only(left:6.0),
+                    child: Icon(Icons.people_outline_outlined,
+                      color: (filterBy == "Mutual")? Color(figmaColours.highlight) : Colors.white,
+                    ),
+                  ),
 
-                label: Text("Mutual   ",
-                  style: TextStyle(
-                      color: (filterBy == "Mutual")? Color(figmaColours.highlight) : Colors.white
+                  label: Text(" Mutual   ",
+                    style: TextStyle(
+                        color: (filterBy == "Mutual")? Color(figmaColours.highlight) : Colors.white
+                    ),
                   ),
+                  onPressed: () {
+                    setState(() {filterBy = "Mutual";});
+                  },
+                  backgroundColor: Color(figmaColours.greyDark),
                 ),
-                onPressed: () {
-                  setState(() {filterBy = "Mutual";});
-                },
-                backgroundColor: Color(figmaColours.greyDark),
               ),
-              ActionChip(
-                avatar: Icon(Icons.arrow_upward_outlined,
-                  color: (filterBy == "Top")? Color(figmaColours.highlight) : Colors.white,
-                ),
-                label: Text("Top   ",
-                  style: TextStyle(
-                    color: (filterBy == "Top")? Color(figmaColours.highlight) : Colors.white
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: ActionChip(
+                  side: BorderSide(width: 0),
+                  avatar: Padding(
+                    padding: const EdgeInsets.only(left: 6.0),
+                    child: Icon(Icons.arrow_upward_outlined,
+                      color: (filterBy == "Top")? Color(figmaColours.highlight) : Colors.white,
+                    ),
                   ),
+                  label: Text("Top   ",
+                    style: TextStyle(
+                      color: (filterBy == "Top")? Color(figmaColours.highlight) : Colors.white
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {filterBy = "Top";});
+                  },
+                  backgroundColor: Color(figmaColours.greyDark),
                 ),
-                onPressed: () {
-                  setState(() {filterBy = "Top";});
-                },
-                backgroundColor: Color(figmaColours.greyDark),
               ),
-              ActionChip(
-                avatar: Icon(Icons.date_range_outlined,
-                  color: (filterEndDate != null)? Color(figmaColours.highlight) : Colors.white,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: ActionChip(
+                  labelPadding: EdgeInsets.only(left: 4),
+                  padding: const EdgeInsets.only(left: 8.0),
+                  side: BorderSide(width: 0),
+                  avatar: Icon(Icons.date_range_outlined,
+                    color: (filterEndDate != null)? Color(figmaColours.highlight) : Colors.white,
+                  ),
+                  label: Text('Date     ',
+                    style: TextStyle(
+                        color: (filterEndDate != null)? Color(figmaColours.highlight) : Colors.white
+                    ),),
+                  onPressed: () {},
+                  backgroundColor: Color(figmaColours.greyDark),
                 ),
-                label: Text('Date   ',
-                  style: TextStyle(
-                      color: (filterEndDate != null)? Color(figmaColours.highlight) : Colors.white
-                  ),),
-                onPressed: () {},
-                backgroundColor: Color(figmaColours.greyDark),
-              )
+              ),
             ],
           ),
-        )
+        ),
+        SfDateRangePicker(),
       ],
     );
   }
