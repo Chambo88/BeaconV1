7ff95ba435957eab52810292f987d556824bb26e
Chambo88 <benchamberlain88@gmail.com>
2022-02-16 16:47:19 +1300

    updated event tiles

--- lib/widgets/tiles/EventTile.dart
+++ lib/widgets/tiles/EventTile.dart
@@ -67,153 +67,171 @@ class EventTile extends StatelessWidget {
           ],
         ),
       ),
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => EventDetailsPage(event: event)));
-        },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => EventDetailsPage(event: event)));
+          },
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  event.eventName + " -",
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                  textAlign: TextAlign.center,
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        event.eventName + " -",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0),
+                      child: Text(
+                        event.hostName,
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                    Row(children: [
+                      Container(
+                        width: 85,
+                        height: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                                image: NetworkImage(event.imageUrl),
+                                fit: BoxFit.cover)),
+                      ),
+                      Expanded(
+                        flex: 6,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0),
+                                  child: Icon(Icons.date_range),
+                                ),
+                                Flexible(
+                                  child: Container(
+                                    child: Text(
+                                      getDateText(event.startTime)
+                                          .replaceAll("", "\u{200B}"),
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      softWrap: false,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Container(
+                              height: 8,
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0),
+                                  child: Icon(Icons.location_on_outlined),
+                                ),
+                                Flexible(
+                                  child: Container(
+                                    child: Text(
+                                      event.locationName
+                                          .replaceAll("", "\u{200B}"),
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      softWrap: false,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0),
+                                  child: Icon(Icons.music_note_outlined),
+                                ),
+                                Text(
+                                  event.mainGenre,
+                                  style: Theme.of(context).textTheme.headline6,
+                                )
+                              ],
+                            ),
+                            Container(
+                              height: 8,
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0),
+                                  child: Icon(Icons.people_alt_outlined),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                      text: (mutualFriendCount != '0')
+                                          ? "${mutualFriendCount}m"
+                                          : '',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Color(figmaColours.highlight),
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                          text: (mutualFriendCount != '0')
+                                              ? " | ${event.usersAttending.length.toString()}"
+                                              : "${event.usersAttending.length.toString()}",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline6,
+                                        )
+                                      ]),
+                                  textWidthBasis: TextWidthBasis.longestLine,
+                                  overflow: TextOverflow.ellipsis,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ]),
+                  ],
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(bottom: 4.0),
-                child: Text(
-                  event.hostName,
-                  style: Theme.of(context).textTheme.bodyText2,
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Divider(
+                  height: 1,
+                  color: Color(figmaColours.greyLight),
                 ),
-              ),
-              Row(children: [
-                Container(
-                  width: 85,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      image: DecorationImage(
-                          image: NetworkImage(event.imageUrl),
-                          fit: BoxFit.cover)),
-                ),
-                Expanded(
-                  flex: 6,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Icon(Icons.date_range),
-                          ),
-                          Flexible(
-                            child: Container(
-                              child: Text(
-                                getDateText(event.startTime)
-                                    .replaceAll("", "\u{200B}"),
-                                style: Theme.of(context).textTheme.headline6,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                softWrap: false,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Container(
-                        height: 8,
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Icon(Icons.location_on_outlined),
-                          ),
-                          Flexible(
-                            child: Container(
-                              child: Text(
-                                event.locationName.replaceAll("", "\u{200B}"),
-                                style: Theme.of(context).textTheme.headline6,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                softWrap: false,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Icon(Icons.music_note_outlined),
-                          ),
-                          Text(
-                            event.mainGenre,
-                            style: Theme.of(context).textTheme.headline6,
-                          )
-                        ],
-                      ),
-                      Container(
-                        height: 8,
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Icon(Icons.people_alt_outlined),
-                          ),
-                          RichText(
-                            text: TextSpan(
-                                text: (mutualFriendCount != '0')
-                                    ? "${mutualFriendCount}m"
-                                    : '',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Color(figmaColours.highlight),
-                                ),
-                                children: [
-                                  TextSpan(
-                                    text: (mutualFriendCount != '0')
-                                        ? " | ${event.usersAttending.length.toString()}"
-                                        : "${event.usersAttending.length.toString()}",
-                                    style:
-                                        Theme.of(context).textTheme.headline6,
-                                  )
-                                ]),
-                            textWidthBasis: TextWidthBasis.longestLine,
-                            overflow: TextOverflow.ellipsis,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ]),
+              )
             ],
           ),
         ),
